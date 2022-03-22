23702e6e8eb04118a120cca55b0f9289687df73b
Nurzhan Nursultanov <nursultanov.nurzhan@gmail.com>
2022-03-22 13:55:11 +1300

    added routing and game-resource

---
+++ bff/game-resource.go
@@ -0,0 +1,130 @@
+package bff
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	pbgameengine "github.com/kazakh-in-nz/m-game-engine/v1"
+	pbhighscore "github.com/kazakh-in-nz/m_apis/v1"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+)
+
+type gameResource struct {
+	gameClient       pbhighscore.GameClient
+	gameEngineClient pbgameengine.GameEngineClient
+}
+
+func NewGameResource(gameClient pbhighscore.GameClient, gameEngineClient pbgameengine.GameEngineClient) *gameResource {
+	return &gameResource{
+		gameClient:       gameClient,
+		gameEngineClient: gameEngineClient,
+	}
+}
+
+func NewGrpcGameServiceClient(serverAddr string) (pbhighscore.GameClient, error) {
+	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatal().Msg(fmt.Sprintf("Failed to dial: %v", err))
+		return nil, err
+	} else {
+		log.Info().Msgf("Successfully connected to [%s]", serverAddr)
+
+	}
+
+	if conn == nil {
+		log.Info().Msg("m-highscore connection is nil in m-bff")
+	}
+
+	client := pbhighscore.NewGameClient(conn)
+
+	return client, nil
+
+}
+
+func NewGrpcGameEngineServiceClient(serverAddr string) (pbgameengine.GameEngineClient, error) {
+	conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatal().Msg(fmt.Sprintf("Failed to dial: %v", err))
+		return nil, err
+	} else {
+		log.Info().Msgf("Successfully connected to [%s]", serverAddr)
+
+	}
+
+	if conn == nil {
+		log.Info().Msg("m-game-engine connection is nil in m-bff")
+	}
+
+	client := pbgameengine.NewGameEngineClient(conn)
+
+	return client, nil
+
+}
+
+func (gr *gameResource) SetHighScore(c *gin.Context) {
+	highScoreString := c.Param("hs")
+	highScoreFloat64, err := strconv.ParseFloat(highScoreString, 64)
+
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to convert highscore to float")
+	}
+
+	_, error := gr.gameClient.SetHighScore(context.Background(), &pbhighscore.SetHighScoreRequest{
+		HighScore: highScoreFloat64,
+	})
+
+	if error != nil {
+		log.Error().Err(err).Msg("Error while setting high score in m-highscore")
+	}
+}
+
+func (gr *gameResource) GetHighScore(c *gin.Context) {
+	highScoreResponse, err := gr.gameClient.GetHighScore(context.Background(), &pbhighscore.GetHighScoreRequest{})
+
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to get high score")
+	}
+
+	hsString := strconv.FormatFloat(highScoreResponse.GetHighScore(), 'e', -1, 64)
+
+	c.JSONP(200, gin.H{
+		"hs": hsString,
+	})
+}
+
+func (gr *gameResource) GetSize(c *gin.Context) {
+	sizeResponse, err := gr.gameEngineClient.GetSize(context.Background(), &pbgameengine.GetSizeRequest{})
+
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to get size")
+	}
+
+	sizeString := strconv.FormatFloat(sizeResponse.GetSize(), 'e', -1, 64)
+
+	c.JSONP(200, gin.H{
+		"hs": sizeString,
+	})
+}
+
+func (gr *gameResource) SerScore(c *gin.Context) {
+	scoreString := c.Param("score")
+	score64, err := strconv.ParseFloat(scoreString, 64)
+
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to convert score to float")
+	}
+
+	_, error := gr.gameEngineClient.SetScore(context.Background(), &pbgameengine.SetScoreRequest{
+		Score: score64,
+	})
+
+	if error != nil {
+		log.Error().Err(err).Msg("Error while setting score in m-game-engine")
+	}
+}
---
+++ cli/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kazakh-in-nz/m-bff/bff"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	grpcAddressHighScore := flag.String("address-m-highscore", "localhost:60051", "The grpc server address for highscore service")
+	grpcAddressGameEngine := flag.String("address-m-highscore", "localhost:60052", "The grpc server address for game engine service")
+
+	serverAddress := flag.String("address-http", ":8081", "HTTP server address")
+
+	flag.Parse()
+
+	gameClient, errGameClient := bff.NewGrpcGameServiceClient(*grpcAddressHighScore)
+	if errGameClient != nil {
+		log.Error().Err(errGameClient).Msg("Error in creating a client for m-highscore")
+	}
+
+	gameEngineClient, errGameEngineClient := bff.NewGrpcGameEngineServiceClient(*grpcAddressGameEngine)
+	if errGameEngineClient != nil {
+		log.Error().Err(errGameEngineClient).Msg("Error in creating a client for m-game-engine")
+	}
+
+	gr := bff.NewGameResource(gameClient, gameEngineClient)
+
+	router := gin.Default()
+
+	router.GET("/geths", gr.GetHighScore)
+	router.POST("/seths/:hs", gr.SetHighScore)
+	router.GET("/getsize", gr.GetSize)
+	router.POST("/setscore/:score", gr.SerScore)
+
+	err := router.Run(*serverAddress)
+
+	if err != nil {
+		log.Fatal().Err(err).Msg("Could not start bff")
+	}
+
+	log.Info().Msgf("Started http-server at %v", *serverAddress)
+}
